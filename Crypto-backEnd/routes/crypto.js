9f5738782397e365ab0cc4e72023ea0764efa397
rKaushalya <ravinduk56@gmail.com>
2024-09-14 03:02:36 +0530

    new data added

--- Crypto-backEnd/routes/crypto.js
+++ Crypto-backEnd/routes/crypto.js
@@ -6,6 +6,7 @@ const prices = [
     { name: 'Bitcoin', symbol: 'BTC', price: '$27,423.23', change: '+3.45%' },
     { name: 'Ethereum', symbol: 'ETH', price: '$1,723.50', change: '-1.12%' },
     { name: 'Ripple', symbol: 'XRP', price: '$0.52', change: '+2.08%' },
+    { name: 'Ripple', symbol: 'XRP', price: '$0.52', change: '+2.08%' },
 ];
 
 // API route to return the prices data
